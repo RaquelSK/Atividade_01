7929451e7c7003013bd3bca24bd99bfd0eb49318
RaquelSK <raquelkouri366@gmail.com>
2025-03-13 23:40:45 -0300

    Create indexx.js
    atividade_01

---
+++ indexx.js
@@ -0,0 +1,29 @@
+import { soma } from "./soma.js";
+ import { subtrair } from "./subtrair.js";
+ import { dividir } from "./dividir.js";
+ import { multiplicar } from "./multiplicar.js";
+ 
+ const num1 = 8;
+ const num2 = 2;
+ const operaçao = "/";
+ let result;
+ switch (operaçao) {
+     case "+":
+         result = soma(num1,num2);
+         break;
+     case "-":
+     result = subtrair (num1,num2);
+     break;
+     case "*":
+         result = multiplicar (num1,num2);
+         break;
+     case "/":
+         result = dividir (num1,num2);
+         break;
+         default:
+             console.log("Operação invalida.");
+             result = null;
+ }
+ if (result !== null) {
+     console.log(`O resultado da operação ${num1} ${operaçao} ${num2} é: ${result}`);
+ }
